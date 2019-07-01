42a01c277a3c771620a0f72204083ba277fd6e36
1255542159 <1255542159@qq.com>
2019-07-01 15:55:29 +0800

    My_Account/My_Contact/My_PassWord的创建

---
+++ app/src/main/java/com/example/a12306/my/MyAccount.java
@@ -0,0 +1,15 @@
+package com.example.a12306.my;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.a12306.R;
+
+public class MyAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_account);
+    }
+}
