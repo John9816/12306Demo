f664465fe837066d26636fa9ab9f4d383c3125ed
1255542159 <1255542159@qq.com>
2019-07-01 16:51:24 +0800

    2019/7/1/16.45

--- app/src/main/java/com/example/a12306/BootPageActivity.java
+++ app/src/main/java/com/example/a12306/BootPageActivity.java
@@ -2,12 +2,19 @@ package com.example.a12306;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class BootPageActivity extends AppCompatActivity {
+public class BootPageActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boot_page);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
